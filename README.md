aafbbb4907c06214f4429b6769d9d93caeb66884
gDan15 <gaetan.guru@gmail.com>
2018-12-10 16:23:09 +0100

    Modification YAML file
    * .yml file modification
    * add in README

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Scalable_architecture
+    The purpose of this micro service is to save products in a MongoDB database temporarily.
+## Explanations
+    - In order to know precisely what the user wants, a SKU has to be determined.
+      The last is obtained by requesting it to another micro service.
+      For example : Macbook, Pro, i5, 16Go, 256GO SSD, red, ... will give an unique SKU.
+    - In order to know if item is in stock, a request with the SKU number is sent to the micro service Stock.
+    - If product is in stock a confirmation is sent to the micro service Order.
+      When an operation takes place in the card it will notice this to the micro service Order.
+    - When the Cart is validated, the content of the last is transferred to the Order micro service.
 ## Python version
     3.7.1
 ## Installed packages 
--- cart.py
+++ cart.py
@@ -1,3 +1,39 @@
+# System modules
+from datetime import datetime
+
+# 3rd party modules
+from flask import make_response, abort
+
+CART = {
+    "cart1":{
+            "item":
+                {
+                "Id" : "",
+                "Nom" : "Macbook Pro",
+                "Prix" : "2000",
+                "Commentaire" : [
+                    "Very Good", "Love this product"
+                ],
+                "Description" : [
+                    "blue", "Macbook Pro 13 pouce"
+                ]
+               },
+            },
+    "cart2":{
+            "item":{
+                "Id" : "",
+                "Nom" : "Macbook Pro",
+                "Prix" : "2000",
+                "Commentaire" : [
+                    "Very Good", "Love this product"
+                ],
+                "Description" : [
+                    "blue", "Macbook Pro 13 pouce"
+                ]
+              },
+            },
+    }
+
 def readAllItems():
     """
     This function responds to a request for /api/people
@@ -6,4 +42,18 @@ def readAllItems():
     :return:        json string of list of people
     """
     # Create the list of people from our data
-    return [CART[key] for key in sorted(PEOPLE.keys())]
+    return[CART[key] for key in sorted(CART.keys())]
+
+def giveToken(nameOfCart):
+    """
+    This function transmits the token describing the product.
+    The data is given to the micro service Item, in return an Id is given to the item.
+    :return:        json string with 2 keys : 'Name' and "Description"
+    """
+
+    dictionary = CART[nameOfCart]
+
+    return dictionary
+
+
+
