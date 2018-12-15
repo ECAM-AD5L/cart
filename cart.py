5c61184fa9cd628815ebfab46effe6f2057959cb
Gaétan Guru <gaetan.guru@gmail.com>
2018-12-15 21:04:36 +0100

    Request POST for adding dictionary is now working.
    * 'Content-type' of postman in tab 'Headers' has to be set at 'application/json'.

--- cart.py
+++ cart.py
@@ -2,11 +2,11 @@
 from datetime import datetime
 
 # 3rd party modules
-from flask import make_response, abort
+from flask import make_response, abort, request
 
 CART = {
     "cart1":{
-            "item1":
+            "1":
                 {
                 "Id" : "",
                 "Nom" : "Macbook Pro",
@@ -18,7 +18,7 @@ CART = {
                     "blue", "Macbook Pro 13 pouce"
                 ]
                },
-            "item2":
+            "2":
                 {
                 "Id" : "",
                 "Nom" : "Macbook Air",
@@ -32,7 +32,7 @@ CART = {
                },
             },
     "cart2":{
-            "item1":{
+            "1":{
                 "Id" : "",
                 "Nom" : "Macbook Pro",
                 "Prix" : "2000",
@@ -46,7 +46,7 @@ CART = {
             },
     }
 
-def readAllItems():
+def readAllCarts():
     """
     This function responds to a request for /api/people
     with the complete lists of people
@@ -56,7 +56,7 @@ def readAllItems():
     # Create the list of people from our data
     return[CART[key] for key in sorted(CART.keys())]
 
-def giveToken(nameOfCart):
+def readOneCart(nameOfCart):
     """
     This function transmits the token describing the product.
     The data is given to the micro service Item, in return an Id is given to the item.
@@ -69,10 +69,30 @@ def giveToken(nameOfCart):
         # otherwise, nope, not found
     else:
         abort(
-            404, "Cart {nameOfCart} not found".format(nameOfCart=nameOfCart)
+            404, "Cart {nameOfCart} not found.".format(nameOfCart=nameOfCart)
         )
-
     return dictionary
 
+def deleteCart(nameOfCart):
+    """
+    This function deletes a cart according to the parameter nameOfCart.
+    :return:        nothing
+    """
+    if nameOfCart in CART:
+        #TODO : delete dictionary in mongodb
+        print("Cart deleted !")
+    else:
+        abort(
+            404, "Cart {nameOfCart} not found.".format(nameOfCart=nameOfCart)
+        )
+    return "Cart deleted."
+
+def addCart(newCart):
+    """
+    This function adds a cart according to the parameter nameOfCart.
+    :return:        nothing
+    """
+    # TODO : add dictionary in mongodb
+    return newCart
 
 
